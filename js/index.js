a58d573c3bdf2a52137af483ea6dd878de40139d
pengjm18 <peng.jimmy1@gmail.com>
2022-04-05 23:02:48 +0800

    Add weather forecast codes

---
+++ js/index.js
@@ -0,0 +1,100 @@
+const appendWeatherStatus = (function() {
+  return function(text) {
+    document.getElementById('weatherStatusText').textContent = text;
+  };
+}());
+
+const weatherStatusHandler = (function() {
+  let state = 'getAddrDetails';
+  return function(searchAddress) {
+    if (searchAddress.length === 0) {
+      appendWeatherStatus('Address field is empty. Please enter an address.');
+      return false;
+    }
+    switch (state) {
+      case 'getAddrDetails': {
+        $.ajax({
+          url: `https://developers.onemap.sg/commonapi/search?searchVal=${
+              searchAddress}&returnGeom=Y&getAddrDetails=N`,
+          success: function(response) {
+            if (response.results.length === 0) {
+              appendWeatherStatus('Invalid Longitude, Latitude result.');
+              return false;
+            }
+            setTimeout(weatherStatusHandler, 0, {
+              longitude: response.results[0].LONGITUDE,
+              latitude: response.results[0].LATITUDE
+            });
+            state = 'getToken';
+          }
+        });
+        break;
+      }
+      case 'getToken': {
+        const settings = {
+          'url': 'https://developers.onemap.sg/privateapi/auth/post/getToken',
+          'data': {
+            'email': 'peng.jimmy67@gmail.com',
+            'password': 'UdFrDr3F97k6H9dz'
+          },
+          'async': 'true'
+        };
+        $.post(settings).done(function(response) {
+          setTimeout(weatherStatusHandler, 0, {
+            longitude: searchAddress.longitude,
+            latitude: searchAddress.latitude,
+            token: response.access_token,
+          });
+          state = 'getPlanningareaNames';
+        });
+        break;
+      }
+      case 'getPlanningareaNames': {
+        $.ajax({
+          url:
+              `https://developers.onemap.sg/privateapi/popapi/getPlanningarea/?token=${
+                  searchAddress.token}&lat=${searchAddress.latitude}&lng=${
+                  searchAddress.longitude}`,
+          success: function(response) {
+            if (response.length === 0) {
+              appendWeatherStatus('Invalid Town result.');
+              return false;
+            }
+            setTimeout(weatherStatusHandler, 0, {town: response[0].pln_area_n});
+            state = 'getWeatherForecast';
+          }
+        });
+        break;
+      }
+      case 'getWeatherForecast': {
+        $.ajax({
+          url: 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast',
+          success: function(response) {
+            const matchingArea =
+                response.items[0].forecasts.filter(function(matchedArea) {
+                  return matchedArea.area.toUpperCase() === searchAddress.town;
+                });
+            state = 'getAddrDetails';
+            if (matchingArea.length === 0) {
+              appendWeatherStatus(
+                  'Invalid weather forecasts result for the input address.');
+              return false;
+            }
+            appendWeatherStatus(
+                `Location: ${matchingArea[0].area}. Weather Forecast: ${
+                    matchingArea[0].forecast}.`);
+          }
+        });
+        break;
+      }
+    }
+  }
+}());
+
+(function initSubmitButton() {
+  document.getElementById('submitButton')
+      .addEventListener('click', function handleSubmitButtonClick(event) {
+        event.preventDefault();
+        weatherStatusHandler(document.getElementById('addressText').value);
+      }, false);
+}());
