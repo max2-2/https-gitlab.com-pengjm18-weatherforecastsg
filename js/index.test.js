fc94826cf96fb0b96bdd3e944c41a2f67f5df733
pengjm18 <peng.jimmy1@gmail.com>
2022-04-07 00:31:10 +0800

    Add test skeleton

--- js/index.test.js
+++ js/index.test.js
@@ -4,4 +4,12 @@
 
 const index = require('./index');
 
-describe('index', () => {});
+describe('Render index page', () => {
+  test(
+      'Shows correct format message if the address text input is valid',
+      () => {});
+
+  test(
+      'Shows an error message if the address text input is invalid',
+      async () => {});
+});
